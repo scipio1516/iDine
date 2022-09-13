6c0a0cb815f197e96dca168b1048209663500d95
Leo Lai <scipio1516@gmail.com>
2022-09-13 16:09:59 -0500

    finished checkout view.

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -10,17 +10,39 @@ import SwiftUI
 struct CheckoutView: View {
     @EnvironmentObject var order: Order
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tip = 10
+    
     var body: some View {
         let paymentTypes = ["Cash", "Credit", "Debit", "iDine Points"]
+        let tipAmounts = [10, 15, 20, 25, 0]
         
-        VStack {
+        Form {
             Section {
-                Form {
-                    Picker("CHOOSE a payment type. No barter accepted.", selection: $paymentType) {
-                        ForEach(paymentTypes, id: \.self) { payment in
-                            Text(payment)
-                        }
-                    }.pickerStyle(MenuPickerStyle())
+                Picker("CHOOSE a payment type. No barter accepted.", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) { payment in
+                        Text(payment)
+                    }
+                }
+            }
+            Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+            if(addLoyaltyDetails) {
+                TextField("Enter your iDine ID", text: $loyaltyNumber)
+            }
+            
+            Section(header: Text("Add a tip?"))
+            {
+                Picker("Percentage: ", selection: $tip) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("Total price: $420")) {
+                Button("Check Out!") {
+                    
                 }
             }
         }
